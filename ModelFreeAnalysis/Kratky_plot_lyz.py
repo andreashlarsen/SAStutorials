82860b48de76d12c10ebf6dcd1387abad3c32117
Andreas Haahr Larsen <andreas.larsen@sund.ku.dk>
2024-06-04 14:35:16 +0200

    Add files via upload

--- ModelFreeAnalysis/Kratky_plot_lyz.py
+++ ModelFreeAnalysis/Kratky_plot_lyz.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 q,I,sigma = np.genfromtxt('SASDMF2.dat',skip_header=2,skip_footer=4,unpack=True)
 
-background = ??
+background = 0.0
 plt.plot(q,q**2*(I-background),linestyle='none',marker='.',color='red')
 plt.xlabel('q [$\AA^{-1}$]')
 plt.ylabel('q$^2$ I')
